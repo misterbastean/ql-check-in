1c7ef9f7768fd25a9a4946722fa3333fcc8aabcf
misterbastean <misterbastean@gmail.com>
2018-09-19 11:05:33 -0400

    Conditionally render auth links

--- app.js
+++ app.js
@@ -42,6 +42,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+})
 
 // Routes
 app.use(indexRoutes);

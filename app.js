825b08844a03106cc972f878073b41ed10481e4b
misterbastean <misterbastean@gmail.com>
2018-09-19 10:11:33 -0400

    Add User model, initialize Passport

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // TODO
-// - Create punch editing routes and add buttosn to show page
-// - Throw out punch blocks that extend beyond 1 day
+// - Add authentication and authorization to view reports
+// - Add feedback after punches (connect-flash)
 
-const express = require('express'),
-	app = express(),
-	mongoose = require('mongoose'),
-	bodyParser = require('body-parser'),
-	config = require('./config'),
-	methodOverride = require('method-override');
+const express 		= require('express'),
+	app 						= express(),
+	mongoose 				= require('mongoose'),
+	bodyParser 			= require('body-parser'),
+	config 					= require('./config'),
+	methodOverride 	= require('method-override'),
+	passport				= require('passport'),
+	LocalStrategy		= require('passport-local'),
+	User						= require('./models/user');
 
 // Require Routes
 const indexRoutes = require('./routes/index'),
@@ -25,6 +28,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public')); // Publish assets folder
 app.use(methodOverride('_method'));
+
+// Passport config
+app.use(require('express-session')({
+	secret: config.passport.secret,
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // Routes
 app.use(indexRoutes);
